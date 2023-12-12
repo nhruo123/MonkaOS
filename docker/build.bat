ddad23d66ee274c61427f40ed1228ae6c14db15a
nhruo <nhruo123@gmail.com>
2023-12-12 13:43:42 +0200

    add cache to builder image

--- docker/build.bat
+++ docker/build.bat
@@ -1,6 +1,8 @@
 @echo off
 set make_action=%1
 docker run --rm^
+ -v os-builder-cargo-cache:/root/.cargo/^
+ -v os-builder-rustup-cache:/root/.rustup/^
  -v %cd%:/tmp/builder/^
  --env RUSTFLAGS="--remap-path-prefix /tmp/builder=%cd% --remap-path-prefix /root=%USERPROFILE%"^
   os-builder make %make_action%
--- docker/create-builder-image.bat
+++ docker/create-builder-image.bat
@@ -1,1 +1,3 @@
-docker build ./docker --tag os-builder --no-cache 
+docker build ./docker --tag os-builder --no-cache
+docker volume create os-builder-cargo-cache
+docker volume create os-builder-rustup-cache
